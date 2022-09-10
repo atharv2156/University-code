3f447dc8a86a9aaad6e921105a8e92fb64fc94e4
atharv2156 <atharv27vt@gmail.com>
2022-09-10 18:39:43 +0530

    Initial commit

--- Codeblocks/new.cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-cout<<"new file codeblocks trash";
-}
--- R/1.r
+++
@@ -1,8 +0,0 @@
-name = readline(prompt = "Input your name:- ")
-age = readline(prompt = "Input your age:- ")
-print(paste("my name is ",name,"and I am",age,"years old"))
-
-
-
-
-
