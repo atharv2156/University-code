906d2785bec3570488eb10e83a6115d65fe28581
atharv2156 <atharv27vt@gmail.com>
2022-09-23 18:41:09 +0530

    added 30 python course

--- python_problems/bus_problem.py
+++
@@ -1,8 +0,0 @@
-t = int(input())
-for i in range(t):
-    seat = int(input())
-    if seat > 50 and seat <= 100:
-        print('RIGHT')
-    else:
-        print('LEFT')
-        
--- python_problems/wireframe_math.py
+++
@@ -1,6 +0,0 @@
-t = int(input())
-for i in range(t):
-    h , w , cost =map(int,input().split())
-    length = (h*2)+(w*2)
-    cost *= length
-    print(cost)
